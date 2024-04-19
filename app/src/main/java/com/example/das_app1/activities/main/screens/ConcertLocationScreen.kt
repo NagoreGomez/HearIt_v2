0cf7f3ee2ecea50376b88e2214555b34ffe3d508
Nagore <81180039+NagoreGomez@users.noreply.github.com>
2024-04-19 17:52:16 +0200

    comentarios

--- app/src/main/java/com/example/das_app1/activities/main/screens/ConcertLocationScreen.kt
+++ app/src/main/java/com/example/das_app1/activities/main/screens/ConcertLocationScreen.kt
@@ -5,6 +5,15 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.platform.LocalConfiguration
 import com.example.das_app1.activities.main.MainViewModel
 import com.example.das_app1.activities.main.composables.ConcertLocation
+/*************************************************************************
+ ****                      ConcertLocationScreen                       ****
+ *************************************************************************/
+
+/**
+ * ConcertLocationScreen es la pantalla para mostrar la ubicacion del concierto.
+ *
+ * @param mainViewModel [MainViewModel] contiene los estados y llamadas necesarias.
+ */
 
 @Composable
 fun ConcertLocationScreen(
